2078a516ceac724d4b56b132a0bc01b5203660d8
unknown <markw@.core.hsd.com.au>
2011-01-12 18:16:57 +1100

    Started work on basic searcher.

--- src/main/java/au/id/wolfe/recursivefilelister/FileSearcher.java
+++ src/main/java/au/id/wolfe/recursivefilelister/FileSearcher.java
@@ -2,12 +2,15 @@ package au.id.wolfe.recursivefilelister;
 
 import au.id.wolfe.recursivefilelister.impl.DefaultFileLister;
 import au.id.wolfe.recursivefilelister.util.Assert;
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  *
@@ -15,9 +18,14 @@ import java.util.concurrent.Executors;
 public class FileSearcher {
     final static Logger logger = LoggerFactory.getLogger(DefaultFileLister.class);
 
+    BlockingQueue<File> queue = new LinkedBlockingQueue<File>(100);
+
+
     FileLister fileLister;
 
-    ExecutorService executor = Executors.newFixedThreadPool(10);
+    ExecutorService executor = Executors.newFixedThreadPool(20);
+
+    static final int N_CONSUMERS = 10;
 
     public void startMyApplication(String basePath) {
         logger.info("Processing started");
@@ -26,8 +34,29 @@ public class FileSearcher {
 
         Assert.argumentIsTrue("BasePath exists", basePathFile.exists());
 
-        File[] files = fileLister.getFileArrayFromDirectory(new File(basePath));
+        FileFilter filter = new FileFilter() {
+            public boolean accept(File file) {
+                return true;
+            }
+        };
 
+        Future<String> resultFileCrawler = executor.submit(new FileCrawler(queue, filter, basePathFile));
+
+        List<Future<String>> resultIndexerList = Lists.newArrayList();
+
+        for (int i = 0; i < N_CONSUMERS; i++){
+           resultIndexerList.add(executor.submit(new Indexer(queue)));
+        }
+
+        try {
+            logger.info(resultFileCrawler.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        //File[] files = fileLister.getFileArrayFromDirectory(new File(basePath));
 
 
         logger.info("Processing finished");
@@ -46,8 +75,63 @@ public class FileSearcher {
             logger.error("FileSearch directory");
         }
 
+    }
 
+    public class FileCrawler implements Callable<String> {
 
+        private final BlockingQueue<File> fileQueue;
+        private final FileFilter fileFilter;
+        private final File root;
+
+        public FileCrawler(BlockingQueue<File> fileQueue, FileFilter fileFilter, File root) {
+            this.fileQueue = fileQueue;
+            this.fileFilter = fileFilter;
+            this.root = root;
+        }
+
+        public String call() throws Exception {
+            try {
+                crawl(root);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            return "DONE";
+        }
+
+        private void crawl(File root) throws InterruptedException {
+            File[] entries = root.listFiles(fileFilter);
+            if (entries != null) {
+                for (File entry : entries)
+                    if (entry.isDirectory())
+                        crawl(entry);
+                    else
+                        fileQueue.put(entry);
+            }
+        }
+
+    }
+
+    public class Indexer implements Callable<String> {
+        private final BlockingQueue<File> queue;
+
+        public Indexer(BlockingQueue<File> queue) {
+            this.queue = queue;
+        }
+
+        public String call() throws Exception {
+            try {
+                while (true)
+                    indexFile(queue.take());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
+            return "DONE";
+        }
+
+        private void indexFile(File file) throws IOException {
+            logger.info(String.format("%s | %s | %s | %s", file.getParentFile().getCanonicalPath(), file.getName(), file.length(), file.isDirectory()));
+        }
     }
 
 
